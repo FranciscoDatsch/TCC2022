9d6390debf525ef1391740827e0b36317244b624
FranciscoDatsch <fachinettof@gmail.com>
2022-11-15 17:22:52 -0300

    dsa

--- main.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("ligar")
-const titles = document.getElementsByClassName("titulos")
-
-btn.onclick = () => {
-    Array.from(titles).forEach((element, index) => {
-        element.innerHTML = "Funcionou"
-    })
-}
